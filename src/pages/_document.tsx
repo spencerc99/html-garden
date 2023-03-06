fcd7747d083c42bd443fdc6a31913373874e94f9
Spencer Chang <spencerc99@gmail.com>
2023-03-05 17:48:08 -0800

    Initial commit from Create Next App

---
+++ src/pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Main, Head, NextScript } from "next/document";
+export default function Document() {
+  return (
+    <Html data-theme="cmyk">
+      <Head lang="en">
+        <meta charSet="UTF-8" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import styles from "../styles/Home.module.scss";
+import { Head } from "next/document";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="./tk.svg" />
+        <link rel="apple-touch-icon" href="./tk.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,600;1,400&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Special+Elite&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+    </>
+  );
+}
