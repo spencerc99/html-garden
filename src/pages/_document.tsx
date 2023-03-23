4a5d07bdcce834333227d26151303f59332bd782
Spencer Chang <spencerc99@gmail.com>
2023-03-22 18:25:45 -0700

    idk maybe fix apple touch icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default function Document() {
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>🌿</text></svg>"
         ></link>
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
+        <link rel="apple-touch-icon" href="./apple-touch-icon.png"></link>
         {/* <link rel="preconnect" href="https://fonts.googleapis.com" /> */}
         {/* <link
           href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,600;1,400&display=swap"
