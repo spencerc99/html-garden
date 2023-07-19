2484c8bf095328198e1684c6083422e8e28d6b18
Spencer Chang <spencerc99@gmail.com>
2023-07-19 16:06:44 -0700

    add plausible

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,15 +9,11 @@ export default function Document() {
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>🌿</text></svg>"
         ></link>
         <link rel="apple-touch-icon" href="./apple-touch-icon.png"></link>
-        {/* <link rel="preconnect" href="https://fonts.googleapis.com" /> */}
-        {/* <link
-          href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,600;1,400&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Special+Elite&display=swap"
-          rel="stylesheet"
-        /> */}
+        <script
+          defer
+          data-domain="htmlgarden.spencerchang.me"
+          src="https://plausible.io/js/script.js"
+        ></script>
       </Head>
       <body>
         <Main />
